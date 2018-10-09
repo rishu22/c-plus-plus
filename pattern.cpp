0492925e55cbcbf093131fd96323fbdbe1472cc1
rishavkrishna <rishavkrishna22@gmail.com>
2018-10-09 10:45:21 +0530

    cpp

---
+++ pattern.cpp
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+int i,j;
+ 
+
+ for(i=6;i>=0;i--)
+  {
+    for(j=i;j>=0;j--)
+     {
+       printf("%d  ",j);
+     }
+    printf("\n");
+ }
+ getch();
+ }
